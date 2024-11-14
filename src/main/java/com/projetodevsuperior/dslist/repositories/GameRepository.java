8e6285c3855f36ca39b7b49a2a976f9e1bf78a13
João Gabriel <joaogabrielsp11@gmail.com>
2024-11-13 21:07:50 -0300

    Game repository, service, controller

---
+++ src/main/java/com/projetodevsuperior/dslist/repositories/GameRepository.java
@@ -0,0 +1,8 @@
+package com.projetodevsuperior.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.projetodevsuperior.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long>{
+    
+}
---
+++ src/main/java/com/projetodevsuperior/dslist/services/GameService.java
@@ -0,0 +1,23 @@
+package com.projetodevsuperior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetodevsuperior.dslist.dto.GameMinDTO;
+import com.projetodevsuperior.dslist.entities.Game;
+import com.projetodevsuperior.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+    }
+}
