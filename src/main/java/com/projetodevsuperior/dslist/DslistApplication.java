e19ead1cd074984128f051126c6501ba094a412b
João Gabriel <joaogabrielsp11@gmail.com>
2025-02-17 21:18:10 -0300

    Deploy ready

--- src/main/java/com/projetodevsuperior/dslist/DslistApplication.java
+++ src/main/java/com/projetodevsuperior/dslist/DslistApplication.java
@@ -3,11 +3,14 @@ package com.projetodevsuperior.dslist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DslistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DslistApplication.class, args);
+		Dotenv dotenv = Dotenv.load();
 	}
 
 }
